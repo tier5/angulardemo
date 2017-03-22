6970670e7bb3ae6e65302b23c60d7a8ae1aa653e
tier5 <work@tier5.us>
2017-03-22 20:53:18 +0530

    added click functionality

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,4 +1,10 @@
 'use strict';
-angular.module('blogList').controller('BlogListController',function(){
+angular.module('blogList').controller('BlogListController',function($scope){
 	console.log('i am here');
+	$scope.title="Title"
+	$scope.clicks=0
+	$scope.someclicks=function(){
+		$scope.clicks+=1;
+		$scope.title="Title "+$scope.clicks+' times clicked'
+	}
 })
