6e45fe2b0c281d539e1682fc996d2bce29bf88c5
tier5 <work@tier5.us>
2017-03-23 14:03:26 +0530

    Added External template to the page

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('blogList').controller('BlogListController',function($scope){
+/*angular.module('blogList').controller('BlogListController',function($scope){
 	console.log('i am here');
 	$scope.title="Title"
 	$scope.clicks=0
@@ -7,4 +7,19 @@ angular.module('blogList').controller('BlogListController',function($scope){
 		$scope.clicks+=1;
 		$scope.title="Title "+$scope.clicks+' times clicked'
 	}
+})*/
+
+angular.module('blogList').component('blogList',{
+	templateUrl:'./templates/blog-list.html',
+	controller:function($scope){
+		$scope.title="Title"
+		$scope.clicks=0
+
+		$scope.someclicks=function(){
+		$scope.clicks+=1;
+		$scope.title="Title "+$scope.clicks+' times clicked'
+		}
+	}
+
+
 })
